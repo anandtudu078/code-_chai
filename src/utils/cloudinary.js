adfc9f5504483aebbbf06c227736bbb51028bb18
Anand Tudu <anandtudu078@gmail.com>
2026-05-23 22:35:27 +0530

    added the multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,14 @@
+import {v2 as cloud} from 'cloudinary';
+import fs from 'fs';
+
+
+cloudinary.config({ 
+  cloud_name: 'leo_and_code', 
+  api_key: 'my_key', 
+  api_secret: 'my_secret'
+});
+
+
+
+
+
